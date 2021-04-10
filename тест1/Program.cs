2b5f0cfc9cda4c02242e1068f9d2bf0e9a850056
SwenWebber <limpo2095@gmail.com>
2021-04-10 22:03:10 +0500

    new-feature

---
+++ тест1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace тест1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
